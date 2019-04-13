8cce229f0cc2e78c12092bc65d5dad46e70d78bc
Alex <kovacs.alex26@gmail.com>
2019-04-13 11:00:41 +0300

    update the API URL to the new one, switch to node-fetch, fix a few bugs

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
 const fs = require('fs-extra');
-const superagent = require('superagent');
+const fetch = require('node-fetch');
 
 class Checker {
     constructor() {
-        this.api = 'https://fortnite-public-api.theapinetwork.com/prod09/users/id&username=';
+        this.api = 'https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=';
         this._init();
     }
+
+    log(...args) { console.log(...args) }
 
     async _init() {
         let input = await fs.exists('./input.txt');
 
         if (!input) {
-        console.log("Looks like you don't have an input.txt file! Trying to create it...");
-        await fs.ensureFile('./input.txt');
-        return console.log('File has been created, please enter the accounts you want to check! (make sure each one is on a new line)')
+            this.log("Looks like you don't have an input.txt file! Trying to create it...");
+            await fs.ensureFile('./input.txt');
+            return this.log('File has been created, please enter the accounts you want to check! (make sure each one is on a new line)')
         }
 
         input = await fs.readFile('./input.txt');
         input = input.toString();
-        if (input.length <= 1) return console.log("It looks like you haven't entered the accounts you want to check yet!")
 
-        const usernames = input.split('\n');
+        if (input.length <= 1) return this.log("It looks like you haven't entered the accounts you want to check yet!")
+
+        const usernames = input.split('\n').map(u => u.replace('\r', ''));
         const available = new Array();
 
         await this.check(usernames, available);
-        if (available.length < 1) return console.log("I'm sorry to disappoint you, but no account is available.")
+        if (available.length < 1) return this.log("I'm sorry to disappoint you, but no account is available.")
         await fs.outputFile('./output.txt', available.join('\n'));
-        console.log('Verification complete. Please check your output.txt file for all the available accounts!')
+        return this.log('Verification complete. Please check your output.txt file for all the available accounts!')
     }
 
     async check(usernames, available) {
         for (let i = 0; i < usernames.length; i++) {
-            if (usernames[i].length > 16) { console.log(`The ${usernames[i]} username is too long... skipping over it.`); continue; }
-            let { res } = await superagent.get(this.url(usernames[i]));
-            res = JSON.parse(res.text);
+            if (usernames[i].length > 16) { this.log(`The ${usernames[i]} username is longer than the character limit (16), skipping over it.`); continue; }
+            let res = await fetch(this.url(usernames[i]));
+            if (res.status != 200) throw new Error('Something went wrong! The Fortnite API may be currently down');
+            res = await res.json();
             if (res.error && res.errorMessage === 'unknown_epic_user') { available.push(usernames[i]); continue; }
         };
     }
